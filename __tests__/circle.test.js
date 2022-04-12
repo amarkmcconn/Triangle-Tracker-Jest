166e6684939424139ff2e622ebda455c60da47c7
Mark McConnell <amark.mcconnell@gmail.com>
2022-04-12 15:43:17 -0700

    Circle  test 2 good pass.
    Co-authored-by: Jake Edgar <jakeedgar1012@gmail.com>

--- __tests__/circle.test.js
+++ __tests__/circle.test.js
@@ -6,4 +6,9 @@ describe ('Circle', () => {
     const circle = new Circle(2);
     expect(circle.radius).toEqual(2);
   });
+
+  test("if radius input is less than 5 it should return circle object with a size of small", () => {
+    const circle = new Circle(2);
+    expect(circle.sizeOf()).toEqual("small");
+  });
 });
--- src/circle.js
+++ src/circle.js
@@ -1,3 +1,9 @@
 export function Circle(radius){
   this.radius = radius;
 }
+
+Circle.prototype.sizeOf = function() {
+  if (this.radius <= 5){
+    return "small";
+  }
+} 
