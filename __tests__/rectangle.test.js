850ec0c42c8857d5b310ba673c01932b933bb8e8
Mark McConnell <amark.mcconnell@gmail.com>
2022-04-12 14:39:53 -0700

    test 1 & 2 good pass rectangle.
    Co-authored-by: Jake Edgar <jakeedgar1012@gmail.com>

---
+++ __tests__/rectangle.test.js
@@ -0,0 +1,12 @@
+import { Rectangle } from './../src/rectangle.js';
+
+describe('Rectangle', () => {
+
+  test('should correctly create a rectangle object with four lengths', () => {
+    const rectangle = new Rectangle(4,5,4,5);
+    expect(rectangle.side1).toEqual(4);
+    expect(rectangle.side2).toEqual(5);
+    expect(rectangle.side3).toEqual(4);
+    expect(rectangle.side4).toEqual(5);
+  });
+});
